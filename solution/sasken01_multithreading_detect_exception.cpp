aaef6e0103abb8e8ae9951f35a34e7ace061e865
jay Kant Kumar <imjaykant@gmail.com>
2018-06-20 14:19:51 +0530

    Update sasken01_multithreading_detect_exception.cpp

--- solution/sasken01_multithreading_detect_exception.cpp
+++ solution/sasken01_multithreading_detect_exception.cpp
@@ -49,3 +49,42 @@ int main()
     return 0;
     
 }
+
+
+/*--------------------------------------- GDB debugging log ---------------------------------------------
+
+Reading symbols from /home/a.out...done.                                                                                                                                            
+(gdb) r                                                                                                                                                                             
+Starting program: /home/a.out                                                                                                                                                       
+[Thread debugging using libthread_db enabled]                                                                                                                                       
+Using host libthread_db library "/lib/x86_64-linux-gnu/libthread_db.so.1".                                                                                                          
+[New Thread 0x7ffff6fc5700 (LWP 4657)]                                                                                                                                              
+                                                                                                                                                                                    
+Program received signal SIGFPE, Arithmetic exception.                                                                                                                               
+[Switching to Thread 0x7ffff6fc5700 (LWP 4657)]                                                                                                                                     
+0x0000000000400fc7 in thread_function01 () at main.cpp:21                                                                                                                           
+21          cout << 1 / 0 << endl; // Exception code.                                                                                                                               
+(gdb) bt                                                                                                                                                                            
+#0  0x0000000000400fc7 in thread_function01 () at main.cpp:21                                                                                                                       
+#1  0x0000000000402499 in std::_Bind_simple<void (*())()>::_M_invoke<>(std::_Index_tuple<>) (this=0x616c48) at /usr/include/c++/5/functional:1531                                   
+#2  0x0000000000402405 in std::_Bind_simple<void (*())()>::operator()() (                                                                                                           
+    this=0x616c48) at /usr/include/c++/5/functional:1520                                                                                                                            
+#3  0x00000000004023a4 in std::thread::_Impl<std::_Bind_simple<void (*())()> >::_M_run() (this=0x616c30) at /usr/include/c++/5/thread:115                                           
+#4  0x00007ffff793a5b0 in ?? () from /usr/lib/x86_64-linux-gnu/libstdc++.so.6                                                                                                       
+#5  0x00007ffff7bc4182 in start_thread (arg=0x7ffff6fc5700)                                                                                                                         
+    at pthread_create.c:312                                                                                                                                                         
+#6  0x00007ffff73c730d in clone ()                                                                                                                                                  
+    at ../sysdeps/unix/sysv/linux/x86_64/clone.S:111                                                                                                                                
+(gdb) info threads                                                                                                                                                                  
+[New Thread 0x7ffff67c4700 (LWP 4659)]                                                                                                                                              
+  Id   Target Id         Frame                                                                                                                                                      
+  3    Thread 0x7ffff67c4700 (LWP 4659) "a.out" clone ()                                                                                                                            
+    at ../sysdeps/unix/sysv/linux/x86_64/clone.S:81                                                                                                                                 
+* 2    Thread 0x7ffff6fc5700 (LWP 4657) "a.out" 0x0000000000400fc7 in thread_function01 () at main.cpp:21                                                                           
+  1    Thread 0x7ffff7fe8780 (LWP 4653) "a.out" clone ()                                                                                                                            
+    at ../sysdeps/unix/sysv/linux/x86_64/clone.S:81                                                                                                                                 
+(gdb)                                                                                                                                                                               
+      
+
+
+---------------------------------------------------------------------------------------------------------------*/
